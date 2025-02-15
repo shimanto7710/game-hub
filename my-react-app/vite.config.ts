ebc02bd465b836739df07bbfed297dd4e0d2024f
afser <shimanto7710@gmail.com>
2025-02-15 02:48:58 +0100

    deployment

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.ts
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+          router: ['react-router-dom'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Optional: adjust warning limit
+  }
 })
+
